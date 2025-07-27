3b769f9432572fb142905e3d3421daac3bf0e0c9
Jatin Gupta <jating0012@gmail.com>
2025-07-27 13:16:22 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Green_Vision",
+    version="0.1",
+    author="Green Vision Team",
+    author_email="jating0012@gmail.com",    
+    description="A package for Green Vision project",
+    packages= find_packages(),
+    install_requires=[],
+)
+# Run in terminal:
+# python setup.py install
